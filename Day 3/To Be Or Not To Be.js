f1f6f8a1b1befedd64c976745ea576f286c780cb
Lavda <kaustubhwahane55@gmail.com>
2024-04-25 15:33:04 +0530

    Day 5

--- Day 3/To Be Or Not To Be.js
+++ Day 3/To Be Or Not To Be.js
@@ -1,20 +1,19 @@
-/**
- * @param {string} val
- * @return {Object}
- */
-var expect = function(val) {
-  return  function toBe(){
-        if(val === val){
-            console.log({"value": true});
-        } 
-    }
-     
-    function notToBe(){
-        if(val === val){
-            console.log({"value": true});
-        } 
-    }
-};
-
-  expect(5).toBe(5); // true
-  expect(5).notToBe(5); // throws "Equal"
+function expect(val) {
+   return {
+     toBe: function(value) {
+       if (val === value) {
+         return true;
+       } else {
+         throw new Error("Not Equal");
+       }
+     },
+     notToBe: function(value) {
+       if (val !== value) {
+         return true;
+       } else {
+         throw new Error("Equal");
+       }
+     }
+   };
+ }
+ 

38459179ca24b67dc44e8e1bc0bf62f4fbe9d02d
Lavda <kaustubhwahane55@gmail.com>
2024-04-25 16:05:30 +0530

    Aaj thodi mehnat rang laayi

--- Day 4/Counter 2.js
+++ Day 4/Counter 2.js
@@ -3,12 +3,26 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    
+    let initial = init;
+    return {
+        increment: function(){
+          return init  = init + 1;
+        },
+
+        reset: function(){
+            return init = initial;   
+         },
+
+        decrement: function(){
+            return init  = init - 1;
+        },
+       
+    }
+
 };
 
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
+  const counter = createCounter(5)
+  counter.increment(); 
+  counter.reset(); 
+  counter.decrement(); 
+ 

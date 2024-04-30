335fb508e3635264e11eacb2e2f2a6f16ccaf09d
Lavda <kaustubhwahane55@gmail.com>
2024-04-30 17:03:13 +0530

    Array Reduce

--- Day 6/Filter Elements from Array.js
+++ Day 6/Filter Elements from Array.js
@@ -3,12 +3,14 @@
  * @param {Function} fn
  * @return {number[]}
 */
+
+// Case 1 solved
 var filter = function(arr, fn) {
-    let filteredArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if(arr > 10){
-           filteredArr.push(fn[arr[i]]);
-        }
-        return filteredArr;
+  let filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(fn(arr[i]) == true){
+      filteredArr.push(arr[i]);
     }
+  }
+  return filteredArr;
 };
